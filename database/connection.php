b674856670b342b3e7ed7ca3f70854702b41e18d
Gustavo Daniel <gudaniel1@gmail.com>
2023-09-07 10:37:59 -0300

    Ajustes na conexão com o banco

--- database/connection.php
+++ database/connection.php
@@ -1,11 +1,14 @@
 <?php
 $host = $_ENV["DB_HOST"];
+$port = $_ENV["DB_PORT"];
 $database = $_ENV["DB_NAME"];
 $user = $_ENV["DB_USER"];
-$senha = $_ENV["DB_PASS"];
+$password = $_ENV["DB_PASS"];
+
+$dsn = "mysql:host=$host;port=$port;dbname=$database;";
 
 try {
-  $connection = new PDO("mysql:host=$host;dbname=$database;", $user, $senha, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+  $connection = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
   echo 'Falha ao conectar no banco de dados: ' . $e->getMessage();
   die;
