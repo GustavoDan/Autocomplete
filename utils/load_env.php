96c2de6034444e295f0154ebf0b70fa3bfbed9d1
Gustavo Daniel <gudaniel1@gmail.com>
2023-11-10 06:23:18 -0300

    Remoção de variaveis setadas no $_SERVER

--- utils/load_env.php
+++ utils/load_env.php
@@ -7,6 +7,5 @@ if (file_exists($envFilePath)) {
     foreach ($lines as $line) {
         list($key, $value) = explode('=', $line, 2);
         $_ENV[trim($key)] = trim($value);
-        $_SERVER[trim($key)] = trim($value);
     }
 }
